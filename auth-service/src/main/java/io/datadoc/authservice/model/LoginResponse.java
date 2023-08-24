602c476b683bb4d0b62bc982ade9169e26e6c3a2
Boris Skok <skokcmd@gmail.com>
2023-08-25 01:43:45 +0200

    login res docs

--- auth-service/src/main/java/io/datadoc/authservice/model/LoginResponse.java
+++ auth-service/src/main/java/io/datadoc/authservice/model/LoginResponse.java
@@ -1,5 +1,12 @@
 package io.datadoc.authservice.model;
 
+/**
+ * Response from the login endpoint to the client. Contains the JWT tokens & an error object.
+ * Both fields are nullable.
+ *
+ * @param jwtPayload The JWT tokens.
+ * @param loginError The error object.
+ */
 public record LoginResponse(
         JwtPayload jwtPayload,
         LoginError loginError
