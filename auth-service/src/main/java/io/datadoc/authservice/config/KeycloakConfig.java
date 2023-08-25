cf1186e0f41cfa396db54ff8576d9ef6da8f0064
Boris Skok <skokcmd@gmail.com>
2023-08-25 12:54:39 +0200

    updated config javadocs

--- auth-service/src/main/java/io/datadoc/authservice/config/KeycloakConfig.java
+++ auth-service/src/main/java/io/datadoc/authservice/config/KeycloakConfig.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This is a configuration class for the Keycloak.
+ * It provides basic information about the Keycloak instance.
+ */
 @Getter
 @Setter
 @Configuration
--- auth-service/src/main/java/io/datadoc/authservice/config/RestTemplateConfig.java
+++ auth-service/src/main/java/io/datadoc/authservice/config/RestTemplateConfig.java
@@ -5,9 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * This is a configuration class for the RestTemplate.
+ */
 @Configuration
 public class RestTemplateConfig {
 
+  /**
+   * Bean for the RestTemplate - used for making HTTP requests.
+   *
+   * @param builder The RestTemplateBuilder.
+   * @return The RestTemplate.
+   */
   @Bean
   public RestTemplate restTemplate(RestTemplateBuilder builder) {
     return builder.build();
