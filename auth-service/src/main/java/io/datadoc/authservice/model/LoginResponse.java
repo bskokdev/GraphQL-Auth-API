dd73f737b28764be1f7a7c3eb6a7aaefd8ecbaff
Boris Skok <skokcmd@gmail.com>
2023-08-25 12:49:23 +0200

    matched login response to the graphql schema

--- auth-service/src/main/java/io/datadoc/authservice/model/LoginResponse.java
+++ auth-service/src/main/java/io/datadoc/authservice/model/LoginResponse.java
@@ -4,12 +4,12 @@ package io.datadoc.authservice.model;
  * Response from the login endpoint to the client. Contains the JWT tokens & an error object. Both
  * fields are nullable.
  *
- * @param jwtPayload The JWT tokens.
- * @param loginError The error object.
+ * @param payload The JWT tokens.
+ * @param error   The error object.
  */
 public record LoginResponse(
-    JwtPayload jwtPayload,
-    LoginError loginError
+    JwtPayload payload,
+    LoginError error
 ) {
 
 }
