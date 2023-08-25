f73e6f71507bba2b08eb6ac4a2f25a86101c5712
Boris Skok <skokcmd@gmail.com>
2023-08-25 21:35:27 +0200

    made login a  GraphQL mutation

--- auth-service/src/main/java/io/datadoc/authservice/resolver/AuthResolver.java
+++ auth-service/src/main/java/io/datadoc/authservice/resolver/AuthResolver.java
@@ -4,7 +4,7 @@ import io.datadoc.authservice.model.LoginCredentials;
 import io.datadoc.authservice.model.LoginResponse;
 import io.datadoc.authservice.service.AuthService;
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -25,7 +25,7 @@ public class AuthResolver {
    * @param credentials The user's credentials - email & password.
    * @return LoginResponse containing the JWT tokens or an error object.
    */
-  @QueryMapping
+  @MutationMapping
   public LoginResponse login(@Argument LoginCredentials credentials) {
     return this.authService.issueJwtTokensToUser(credentials);
   }
