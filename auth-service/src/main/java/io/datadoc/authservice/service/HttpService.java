113a68bd261e8109b918965ae5f810879bb393f3
Boris Skok <skokcmd@gmail.com>
2023-08-26 18:29:27 +0200

    removed unused header method

--- auth-service/src/main/java/io/datadoc/authservice/service/HttpService.java
+++ auth-service/src/main/java/io/datadoc/authservice/service/HttpService.java
@@ -1,11 +1,8 @@
 package io.datadoc.authservice.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * This is a helper service for making HTTP requests. Provides methods for getting the headers for a
@@ -13,13 +10,6 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class HttpService {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(AuthService.class);
-  private final RestTemplate restTemplate;
-
-  public HttpService(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
 
   /**
    * Get the headers for a request that requires a form body.
@@ -29,19 +19,6 @@ public class HttpService {
   public HttpHeaders getHttpFormHeaders() {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-    return headers;
-  }
-
-  /**
-   * Get the headers for a request that requires a form body and an authorization token.
-   *
-   * @param token The authorization token.
-   * @return The headers for a request that requires a form body and an authorization token.
-   */
-  public HttpHeaders getHttpFormHeaders(String token) {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-    headers.set("Authorization", "Bearer " + token);
     return headers;
   }
 }
